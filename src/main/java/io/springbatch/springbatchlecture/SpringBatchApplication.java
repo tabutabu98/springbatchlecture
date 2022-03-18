ed7a91f21c8e1fe4cac2f2f34007379fbbb2e531
syc <songyechan.kr@gmail.com>
2022-03-18 18:22:05 +0900

    Add: 프로젝트 구성 및 의존성 설정 @tabutabu98
    프로젝트 생성과 의존성 추가.
    강의는 Maven 프로젝트이나 연습은 Gradle로 하는 중.

---
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchApplication.java
@@ -0,0 +1,20 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * 스프링 배치 활성화
+ * 총 4개의 설정 클래스를 실행시키며 스프링 배치의 모든 초기화 및 실행 구성이 이루어진다.
+ * 스프링 부트 배치의 자동 설정 클래스가 실행됨으로 빈으로 등록된 모든 Job을 검색해서 초기화와 동시에 Job을 수행하도록 구성되어있다.
+ */
+@EnableBatchProcessing
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
